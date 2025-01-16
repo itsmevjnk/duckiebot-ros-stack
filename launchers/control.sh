e496a299df0ce2ecba2095349618cf6ba0d4a6f3
Vinh Nguyen <ngtv0404@gmail.com>
2025-01-16 11:09:34 +1100

    disable cmd_vel by default
    this is so that the cmd_vel node would not interfere with other packages, allowing our stack to be used to provide odometry only

--- launchers/control.sh
+++ launchers/control.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch db_bringup bringup.launch control:=false robot_name:=$VEHICLE_NAME
+dt-exec roslaunch db_bringup bringup.launch control:=true robot_name:=$VEHICLE_NAME
 
 
 # ----------------------------------------------------------------------------
